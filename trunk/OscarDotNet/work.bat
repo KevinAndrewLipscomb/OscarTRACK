489ce0025525459f206ba522a48de617055f36d0
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2006-12-27 16:42:45 +0000

    Alpha changes.

--- trunk/OscarDotNet/work.bat
+++ trunk/OscarDotNet/work.bat
@@ -1,8 +1,8 @@
 REM
 REM $Id$
 REM
-cd "C:\Inetpub\wwwroot\KiAspdotnetFramework"
-start /max explorer /e,/select,C:\Inetpub\wwwroot\KiAspdotnetFramework\.svn
-start /max explorer ftp://ftp.frompaper2web.com/ssl/KiAspdotnetFramework_x
-start /max KiAspdotnetFramework.bdsproj
+cd "C:\Inetpub\wwwroot\OscarDotNet"
+start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.svn
+start /max explorer ftp://ftp.frompaper2web.com/ssl/OSCAR_x
+start /max OscarDotNet.bdsproj
 start "" /max "C:\Program Files\MySQL\MySQL Query Browser 1.1\MySQLQueryBrowser.exe"
