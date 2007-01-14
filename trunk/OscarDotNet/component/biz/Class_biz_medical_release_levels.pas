53981ab3d00e78b71033ff6c879013ee6e854c33
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2007-01-14 18:28:02 +0000

    Creation

---
+++ trunk/OscarDotNet/component/biz/Class_biz_medical_release_levels.pas
@@ -0,0 +1,39 @@
+unit Class_biz_medical_release_levels;
+
+interface
+
+uses
+  Class_db_medical_release_levels;
+
+type
+  TClass_biz_medical_release_levels = class
+  private
+    db_medical_release_levels: TClass_db_medical_release_levels;
+  public
+    constructor Create;
+    procedure BindDropDownList
+      (
+      target: system.object;
+      selected_description: string
+      );
+  end;
+
+implementation
+
+constructor TClass_biz_medical_release_levels.Create;
+begin
+  inherited Create;
+  // TODO: Add any constructor code here
+  db_medical_release_levels := TClass_db_medical_release_levels.Create;
+end;
+
+procedure TClass_biz_medical_release_levels.BindDropDownList
+  (
+  target: system.object;
+  selected_description: string
+  );
+begin
+  db_medical_release_levels.BindDropDownList(target,selected_description);
+end;
+
+end.
