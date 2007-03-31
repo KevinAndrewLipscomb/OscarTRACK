e94b21dfb7fb1e5c36f010692c1e8ff398744b8c
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2007-03-31 22:55:49 +0000

    Revised to use robocopy instead of xcopy.

--- deploy.bat
+++ deploy.bat
@@ -2,22 +2,23 @@
 @REM $Id$
 @REM
 @rmdir ready-to-deploy /s /q
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\*.asax ready-to-deploy /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\*.aspx ready-to-deploy /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\*.config ready-to-deploy /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\*.pdf ready-to-deploy /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\bin\*.* ready-to-deploy\bin /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\noninteractive\*.crontab ready-to-deploy\noninteractive /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\noninteractive\*.aspx ready-to-deploy\noninteractive /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\protected\*.asax ready-to-deploy\protected /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\protected\*.aspx ready-to-deploy\protected /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\protected\*.config ready-to-deploy\protected /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\protected\image ready-to-deploy\protected\image /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\protected\template\notification ready-to-deploy\protected\template\notification /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\resource ready-to-deploy\resource /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\usercontrol\app\*.aspx ready-to-deploy\usercontrol\app /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\OscarDotNet\usercontrol\ki\*.aspx ready-to-deploy\usercontrol\ki /a /i /k /y /z
-@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Data.Provider.dll" ready-to-deploy\bin /m /i /k /y /z
-@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Data.Common.dll" ready-to-deploy\bin /m /i /k /y /z
-@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Delphi.dll" ready-to-deploy\bin /m /i /k /y /z
-@xcopy "C:\Program Files\CoreLab\Bdp\MySql\BDS4\CoreLab.Bdp.MySql.dll" ready-to-deploy\bin /m /i /k /y /z
+@REM-cmd--sourcedir----------------------------------------------------------------destdir-----------------------------------------[files]-------------------qualifiers
+@robocopy C:\Inetpub\wwwroot\OscarDotNet                                           ready-to-deploy                                 *.asax                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet                                           ready-to-deploy                                 *.aspx                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet                                           ready-to-deploy                                 *.config                  /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet                                           ready-to-deploy                                 *.pdf                     /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\bin                                       ready-to-deploy\bin                                                       /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\noninteractive                            ready-to-deploy\noninteractive                  *.crontab                 /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\noninteractive                            ready-to-deploy\noninteractive                  *.aspx                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\protected                                 ready-to-deploy\protected                       *.asax                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\protected                                 ready-to-deploy\protected                       *.aspx                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\protected                                 ready-to-deploy\protected                       *.config                  /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\protected\image                           ready-to-deploy\protected\image                                           /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\protected\template\notification           ready-to-deploy\protected\template\notification                           /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\resource                                  ready-to-deploy\resource                                                  /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\usercontrol\app                           ready-to-deploy\usercontrol\app                 *.ascx                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\OscarDotNet\usercontrol\ki                            ready-to-deploy\usercontrol\ki                  *.ascx                    /A /np /z
+@robocopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0" ready-to-deploy\bin                             Borland.Data.Provider.dll /m /np /z
+@robocopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0" ready-to-deploy\bin                             Borland.Data.Common.dll   /m /np /z
+@robocopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0" ready-to-deploy\bin                             Borland.Delphi.dll        /m /np /z
+@robocopy "C:\Program Files\CoreLab\Bdp\MySql\BDS4"                                ready-to-deploy\bin                             CoreLab.Bdp.MySql.dll     /m /np /z
