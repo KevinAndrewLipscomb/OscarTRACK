87715042522e33f28db83a04f32dfb42bcc1b3f0
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2007-05-14 21:58:09 +0000

    Corrected transition table.

--- trunk/OscarDotNet/dbscript/add-suspended-enrollment.sql
+++ trunk/OscarDotNet/dbscript/add-suspended-enrollment.sql
@@ -1,10 +1,19 @@
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT=0;
 START TRANSACTION;
 
+delete from enrollment_level
+where description = 'Suspended';
+
 insert enrollment_level
-  set description = 'Suspended',
-    num_shifts = 0,
-    pecking_order = 95,
-    elaboration = 'as described in Corrective Action Policy';
+set description = 'Suspended',
+  num_shifts = 0,
+  pecking_order = 95,
+  elaboration = 'as described in Corrective Action Policy';
+
+delete from enrollment_transition
+where valid_next_level_code = 19
+  or current_level_code = 19;
 
 insert enrollment_transition (current_level_code,required_historical_level_code,disallowed_historical_level_code,valid_next_level_code) values
 (1,NULL,NULL,19),
@@ -19,21 +28,22 @@ insert enrollment_transition (current_level_code,required_historical_level_code,
 (10,NULL,NULL,19),
 (18,NULL,NULL,19),
 (19,NULL,NULL,1),
-(19,2,NULL,2),
-(19,3,NULL,3),
+(19,2,3,2),
+(19,3,4,3),
 (19,4,NULL,4),
 (19,NULL,NULL,5),
 (19,NULL,NULL,6),
-(19,NULL,NULL,7),
-(19,NULL,NULL,8),
-(19,NULL,NULL,9),
+(19,2,NULL,7),
+(19,2,4,8),
+(19,2,3,9),
 (19,NULL,NULL,10),
 (19,NULL,NULL,11),
-(19,NULL,NULL,12),
+(19,3,NULL,12),
 (19,NULL,NULL,13),
 (19,NULL,NULL,14),
 (19,NULL,NULL,15),
 (19,NULL,NULL,16),
 (19,NULL,NULL,18);
 
+SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
