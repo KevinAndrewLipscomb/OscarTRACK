238b894f4b92e8b5764d6ae11788eab1cd005ea6
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2008-10-27 15:35:53 +0000

    Added DISTINCT keyword to SQL that generates transition radiobuttonlist.  Removed check for disallowed_historical_level_code of Tenured since a check for Life necessarily catches the same situation, and since the check for Tenured inadvertently allows a former Life member to go from Recruit to Regular.

--- trunk/OscarDotNet/dbscript/improve-enrollment-transitions.sql
+++ trunk/OscarDotNet/dbscript/improve-enrollment-transitions.sql
@@ -3,7 +3,6 @@ START TRANSACTION;
 insert ignore enrollment_transition (current_level_code,required_historical_level_code,disallowed_historical_level_code,valid_next_level_code,authorized_tier_id) values
 (20,NULL,NULL,16,2),
 (10,NULL,3,2,2),
-(10,NULL,4,2,2),
 (10,3,4,3,2),
 (10,4,NULL,4,2),
 (10,NULL,NULL,5,2),
