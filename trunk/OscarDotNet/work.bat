f029ec0d39b52843d8730fe6efd7bd610abbe57c
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-08-18 21:19:11 +0000

    Added command to open corresponding aplus.net folder.

--- trunk/OscarDotNet/work.bat
+++ trunk/OscarDotNet/work.bat
@@ -4,5 +4,6 @@ REM
 cd "C:\Inetpub\wwwroot\OscarDotNet"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.svn
 start /max explorer ftp://ftp.frompaper2web.com/ssl/OscarDotNet_x
+start /max explorer ftp://frompaper2web.win.aplus.net/WWWROOT/OscarTRACK/
 start /max OscarDotNet.dproj
 start "" /max "C:\Program Files\MySQL\MySQL Tools for 5.0\MySQLQueryBrowser.exe"
