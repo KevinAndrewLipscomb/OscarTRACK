242e5fef781032cc59f777bb6584ff2038235677
KevinAnLipscomb <kevinandrewlipscomb@vt.edu>
2009-09-22 19:55:18 +0000

    Revised to open SLN file instead of DPROJ file.

--- work.bat
+++ work.bat
@@ -5,5 +5,5 @@ cd "C:\Inetpub\wwwroot\OscarDotNet"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.svn
 start /max explorer ftp://ftp.frompaper2web.com/ssl/OscarDotNet_x
 start /max explorer ftp://frompaper2web.win.aplus.net/WWWROOT/OscarTRACK/
-start /max OscarDotNet.dproj
+start /max OscarDotNet.sln
 start "" /max "C:\Program Files\MySQL\MySQL Tools for 5.0\MySQLQueryBrowser.exe"
