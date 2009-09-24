0f0be496e4f7a19613d69f5a2056b424b052bcd0
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-09-24 00:12:05 +0000

    Corrected AssemblyInfo settings and controlled required files.

--- trunk/OscarDotNet/Properties/AssemblyInfo.cs
+++ trunk/OscarDotNet/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("OscarDotNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("IBM")]
+[assembly: AssemblyCompany("Kalips'o Infogistics LLC")]
 [assembly: AssemblyProduct("OscarDotNet")]
-[assembly: AssemblyCopyright("Copyright © IBM 2009")]
+[assembly: AssemblyCopyright("Copyright © Kalips'o Infogistics LLC 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
