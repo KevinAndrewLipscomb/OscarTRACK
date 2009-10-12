157b6ddef34ec726b6bba5e672d1c0d852c0005a
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-10-12 15:20:27 +0000

    Revised to use WinSCP for FTP.

--- trunk/OscarDotNet/work.bat
+++ trunk/OscarDotNet/work.bat
@@ -3,7 +3,7 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\OscarDotNet"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.svn
-start "" /max "C:\Program Files\FileZilla FTP Client\filezilla.exe" --site=0/ftp.frompaper2web.com\/ssl/OscarDotNet_x
-start "" /max "C:\Program Files\FileZilla FTP Client\filezilla.exe" --site=0/frompaper2web.win.aplus.net\/WWWROOT/OscarTRACK/
+start "" /max "C:\Program Files\WinSCP\WinSCP.exe" alentus.com/ssl/OscarDotNet_x
+start "" /max "C:\Program Files\WinSCP\WinSCP.exe" aplus.net/OscarTRACK/
 start /max OscarDotNet.sln
 start "" /max "C:\Program Files\MySQL\MySQL Tools for 5.0\MySQLQueryBrowser.exe"
