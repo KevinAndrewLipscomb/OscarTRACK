931b37730ca4373eb4b62f50013a4da6f2f39c1c
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-03-19 18:54:25 +0000

    Made improvements and bug fixes to serial indicator control charting.

--- trunk/OscarDotNet/serial_indicator_control_chart.aspx.cs
+++ trunk/OscarDotNet/serial_indicator_control_chart.aspx.cs
@@ -33,43 +33,65 @@ namespace serial_indicator_control_chart
         {
             const int AVERAGE_NUM_MINUTES_PER_MONTH = 43829;
             // takes into account all scheduled leap days
-            SPCTimeVariableControlChart chart;
             serial_indicator_rec_type datum;
-            Queue history;
             uint i;
-            BufferedImage image;
             ((this.Application["spcchartnet_avail"]) as AutoResetEvent).WaitOne();
             // One data point per month
             // months wide
             // time distance between data points
-            chart = new SPCTimeVariableControlChart(SPCControlChartData.INDIVIDUAL_RANGE_CHART, 1, int.Parse(ConfigurationManager.AppSettings["serial_indicator_control_chart_width_in_months"]), AVERAGE_NUM_MINUTES_PER_MONTH);
-            chart.AutoScroll = true;
+            var width_in_months = int.Parse(ConfigurationManager.AppSettings["serial_indicator_control_chart_width_in_months"]);
+            var chart = new SPCTimeVariableControlChart(SPCControlChartData.INDIVIDUAL_RANGE_CHART, 1, width_in_months, AVERAGE_NUM_MINUTES_PER_MONTH);
             chart.Bounds = new Rectangle(0, 0, 781, 417);
             SPCControlChartData.DefaultSampleValueString = k.EMPTY;
+            chart.ChartAlarmEmphasisMode = SPCChartBase.ALARM_HIGHLIGHT_SYMBOL;
             chart.ChartData.DefectiveDecimalPrecision = 0;
             chart.EnableAlarmStatusValues = false;
             chart.EnableCalculatedValues = false;
             chart.EnableNotes = false;
+            chart.EnableScrollBar = true;
             chart.EnableTimeValues = false;
             chart.EnableTotalSamplesValues = false;
             chart.HeaderStringsLevel = SPCControlChartData.HEADER_STRINGS_LEVEL0;
+            chart.PrimaryChart.ControlLineMode = SPCChartObjects.CONTROL_LINE_VARIABLE;
             chart.PrimaryChart.DisplayChart = true;
             chart.TextRenderingHint = TextRenderingHint.ClearTypeGridFit;
-            history = new TClass_biz_agencies().SerialIndicatorData(k.Safe(this.Request["indicator"], k.safe_hint_type.ECMASCRIPT_WORD), k.Safe(this.Request["agency"], k.safe_hint_type.NUM), k.Safe(this.Request["be_agency_applicable"], k.safe_hint_type.NUM));
+            var history = new TClass_biz_agencies().SerialIndicatorData(k.Safe(this.Request["indicator"], k.safe_hint_type.ECMASCRIPT_WORD), k.Safe(this.Request["agency"], k.safe_hint_type.NUM), k.Safe(this.Request["be_agency_applicable"], k.safe_hint_type.NUM));
             uint history_count = (uint)(history.Count);
             if (history_count > 0)
             {
                 for (i = 0; i <= (history_count - 1); i ++ )
                 {
                     datum = ((serial_indicator_rec_type)(history.Dequeue()));
+                    if (i == 0)
+                      {
+                      chart.ChartData.SetControlLimitValues(new double[] {datum.value,datum.value,datum.value});
+                      }
                     chart.ChartData.AddNewSampleRecord(new ChartCalendar((int)(datum.year), (int)(datum.month), 1), new DoubleArray(new double[] {datum.value}));
+                    if (i > 0)
+                      {
+                      chart.AutoCalculatePrimaryControlLimits();
+                      }
                 }
             }
-            chart.AutoCalculatePrimaryControlLimits();
             chart.AutoScalePrimaryChartYRange();
             chart.ChartData.SetSampleRowHeaderString(0, k.EMPTY);
+            chart.HScrollBar1.Maximum = (int)(history_count);
+            chart.HScrollBar1.LargeChange = width_in_months;
+            int potential_scrollbar_value = chart.HScrollBar1.Maximum - chart.HScrollBar1.LargeChange;
+            if (potential_scrollbar_value < chart.HScrollBar1.Minimum)
+              {
+              chart.HScrollBar1.Value = chart.HScrollBar1.Minimum;
+              }
+            else if (potential_scrollbar_value > chart.HScrollBar1.Maximum)
+              {
+              chart.HScrollBar1.Value = chart.HScrollBar1.Maximum;
+              }
+            else
+              {
+              chart.HScrollBar1.Value = potential_scrollbar_value;
+              }
             chart.RebuildChartUsingCurrentData();
-            image = new BufferedImage(chart, ImageFormat.Jpeg);
+            var image = new BufferedImage(chart, ImageFormat.Jpeg);
             image.JpegImageQuality = 100;
             image.SaveImage(this.Response.OutputStream);
             ((this.Application["spcchartnet_avail"]) as AutoResetEvent).Set();
