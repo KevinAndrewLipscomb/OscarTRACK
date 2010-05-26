b4a04ef31dbc8bacf5061cc9e56fab90213b6802
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-05-26 20:04:06 +0000

    Revised to use built-in FTP instead of WinSCP.

--- trunk/OscarDotNet/work.bat
+++ trunk/OscarDotNet/work.bat
@@ -3,7 +3,7 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\OscarDotNet"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.svn
-start "" /max "C:\Program Files\WinSCP\WinSCP.exe" alentus.com/ssl/OscarDotNet_x
-start "" /max "C:\Program Files\WinSCP\WinSCP.exe" aplus.net/ssl/OscarTRACK/
+start /max explorer ftp://kalipso:@frompaper2web.com/ssl/OscarDotNet_x
+start /max explorer ftp://frompaper2web:@frompaper2web.win.aplus.net/WWWROOT/OscarTRACK/
 start /max OscarDotNet.sln
 start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 OSS\MySQLWorkbench.exe"
