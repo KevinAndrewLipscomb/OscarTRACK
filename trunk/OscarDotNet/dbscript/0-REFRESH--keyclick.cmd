cdf862c5817905f2d9bc53a73cd0a22dc7f7afde
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-08-19 19:01:16 +0000

    Revised to work on WinXP as well as Win7 by deferring to a junction point.

--- trunk/OscarDotNet/dbscript/0-REFRESH--keyclick.cmd
+++ trunk/OscarDotNet/dbscript/0-REFRESH--keyclick.cmd
@@ -7,5 +7,5 @@ if "%1"=="x" set the_user=kalipso
 echo on
 mysql --host=%the_host% --user=%the_user% --password=%2 --execute="drop schema keyclick_%1"
 mysql --host=%the_host% --user=%the_user% --password=%2 --execute="create schema keyclick_%1"
-mysql --host=%the_host% --user=%the_user% --password=%2 --database=keyclick_%1 <"%USERPROFILE%\My Documents\SANDBOX\vocational\kalipso-infogistics\db-dump\keyclick.sql"
+mysql --host=%the_host% --user=%the_user% --password=%2 --database=keyclick_%1 <"%USERPROFILE%\my-documents\SANDBOX\vocational\kalipso-infogistics\db-dump\keyclick.sql"
 mysql --host=%the_host% --user=%the_user% --password=%2 --database=keyclick_%1 <0-render-safe-for-d-or-x--keyclick.sql
