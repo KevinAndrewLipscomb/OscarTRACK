6de7f35960a18fdd70981a534194e32b4a29cd81
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-08-19 18:44:07 +0000

    Revised to work on WinXP and Win7 by deferring to a junction point.

--- trunk/OscarDotNet/dbscript/0-REFRESH.cmd
+++ trunk/OscarDotNet/dbscript/0-REFRESH.cmd
@@ -7,5 +7,5 @@ if "%1"=="x" set the_user=kalipso5
 echo on
 mysql --host=%the_host% --user=%the_user% --password=%2 --execute="drop schema oscardotnetdb_%1"
 mysql --host=%the_host% --user=%the_user% --password=%2 --execute="create schema oscardotnetdb_%1"
-mysql --host=%the_host% --user=%the_user% --password=%2 --database=oscardotnetdb_%1 <"%USERPROFILE%\My Documents\SANDBOX\vocational\kalipso-infogistics\db-dump\oscardotnetdb.sql"
+mysql --host=%the_host% --user=%the_user% --password=%2 --database=oscardotnetdb_%1 <"%USERPROFILE%\my-documents\SANDBOX\vocational\kalipso-infogistics\db-dump\oscardotnetdb.sql"
 mysql --host=%the_host% --user=%the_user% --password=%2 --database=oscardotnetdb_%1 <0-render-safe-for-d-or-x.sql
