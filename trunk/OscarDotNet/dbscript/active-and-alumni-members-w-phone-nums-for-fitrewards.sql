9c60aa2a990bc2a5b62663d69a32dd7cb24780aa
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-09-16 15:36:32 +0000

    Revised to include records with no phone nums since vendor says that is not actually a required field.

--- trunk/OscarDotNet/dbscript/active-and-alumni-members-w-phone-nums-for-fitrewards.sql
+++ trunk/OscarDotNet/dbscript/active-and-alumni-members-w-phone-nums-for-fitrewards.sql
@@ -10,7 +10,7 @@ select concat_ws(
   "",
   "USA",
   IFNULL(email_address,""),
-  phone_num,
+  IFNULL(phone_num,""),
   "",
   IF(enrollment_level.description = "Retired","Alumni","Active")
   )
@@ -33,6 +33,5 @@ from member
   join enrollment_level on (enrollment_level.code=enrollment_history.level_code)
 where cad_num is not null
   and cad_num not like "9%"
-  and phone_num is not NULL
   and enrollment_level.description in ("Applicant","Associate","Regular","Life","Tenured","Atypical","Recruit","Admin","Reduced (1)","Reduced (2)","Reduced (3)","SpecOps","Transferring","Suspended","New trainee","Retired")
 order by cad_num
