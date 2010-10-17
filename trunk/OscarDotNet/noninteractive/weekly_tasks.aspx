1baaaea27dffd1a6dd77711b212ad16e3f3e77f8
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-10-17 20:39:14 +0000

    Added code to scrub keyclick resident table to application standards on a weekly basis.  Reduced height of textboxes on scene_visits_to_love_letter_targets control.

---
+++ trunk/OscarDotNet/noninteractive/weekly_tasks.aspx
@@ -0,0 +1,14 @@
+<%@ Page language="c#" Debug="true" Codebehind="weekly_tasks.aspx.cs" AutoEventWireup="True" Inherits="weekly_tasks.TWebForm_weekly_tasks" %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+  <head>
+    <title></title>
+  </head>
+
+  <body bgcolor="white">
+     <form runat="server">
+     </form>
+  </body>
+</html>
+
---
+++ trunk/OscarDotNet/noninteractive/weekly_tasks.aspx.cs
@@ -0,0 +1,37 @@
+using Class_biz_scheduled_tasks;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace weekly_tasks
+{
+    public partial class TWebForm_weekly_tasks: System.Web.UI.Page
+    {
+        // / <summary>
+        // / Required method for Designer support -- do not modify
+        // / the contents of this method with the code editor.
+        // / </summary>
+        private void InitializeComponent()
+        {
+        }
+
+        protected void Page_Load(object sender, System.EventArgs e)
+        {
+            new TClass_biz_scheduled_tasks().DoWeeklyChores();
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            // Required for Designer support
+            InitializeComponent();
+            base.OnInit(e);
+        }
+
+    } // end TWebForm_weekly_tasks
+
+}
