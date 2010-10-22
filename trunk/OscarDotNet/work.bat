68444f5365c5a8fe249e6495a7f58758e3d3f98a
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-10-22 19:08:08 +0000

    Updated MySQL Workbench path.

--- trunk/OscarDotNet/work.bat
+++ trunk/OscarDotNet/work.bat
@@ -6,4 +6,4 @@ start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.svn
 start /max explorer ftp://kalipso:@frompaper2web.com/ssl/OscarDotNet_x
 start /max explorer ftp://frompaper2web:@frompaper2web.win.aplus.net/WWWROOT/OscarTRACK/
 start /max OscarDotNet.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 OSS\MySQLWorkbench.exe"
+start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 CE\MySQLWorkbench.exe"
