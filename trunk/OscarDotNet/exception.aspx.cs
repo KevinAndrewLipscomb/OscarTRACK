0e3aa80c082b7112a441ab5c5f05bebcaf1c9ebc
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-11-05 18:29:07 +0000

    Revised to send the entire last-exception object to EscalatedException instead of just the base exception object.

--- trunk/OscarDotNet/exception.aspx.cs
+++ trunk/OscarDotNet/exception.aspx.cs
@@ -36,8 +36,9 @@ namespace exception
             {
                 case nature_of_visit_type.VISIT_INITIAL:
                     Title.InnerText = Server.HtmlEncode(ConfigurationManager.AppSettings["application_name"]) + " - exception";
-                    System.Exception the_exception = Server.GetLastError().GetBaseException();
-                    if (Regex.IsMatch(the_exception.Message, "Connection.*to MySQL server", RegexOptions.IgnoreCase) || (the_exception.Message == "Connection open failed. Too many connections"))
+                    var last_error = Server.GetLastError();
+                    var base_exception = last_error.GetBaseException();
+                    if (Regex.IsMatch(base_exception.Message, "Connection.*to MySQL server", RegexOptions.IgnoreCase) || (base_exception.Message == "Connection open failed. Too many connections"))
                     {
                         Table_oops.Visible = false;
                     }
@@ -45,7 +46,7 @@ namespace exception
                     {
                         Table_db_down.Visible = false;
                         Focus(TextArea_user_comment, true);
-                        p.notification_message = k.EscalatedException(the_exception, User.Identity.Name, Session);
+                        p.notification_message = k.EscalatedException(last_error, User.Identity.Name, Session);
                     }
                     break;
                 case nature_of_visit_type.VISIT_POSTBACK_STANDARD:
