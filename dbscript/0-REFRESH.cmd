617ef74f26304783b1e7e53172730ae321233cfd
KevinAnLipscomb <kevinandrewlipscomb@vt.edu>
2010-11-12 18:53:10 +0000

    Revised 0-REFRESH scripts to work for legacy server arrangement as well as cloudserver arrangement.

--- dbscript/0-REFRESH.cmd
+++ dbscript/0-REFRESH.cmd
@@ -1,11 +1,20 @@
 @REM $Id$
 @REM
+set base_db_name=oscardotnetdb
 set the_host=localhost
 set the_user=root
+if "%COMPUTERNAME%"=="alpha" goto :OPS
 if "%1"=="x" set the_host=elm.phpwebhosting.com
 if "%1"=="x" set the_user=kalipso5
+:OPS
+if "%1"=="d" set db_instance=%base_db_name%_d
+if "%1"=="x" set db_instance=%base_db_name%_x
+if "%1"=="p" set db_instance=%base_db_name%
 echo on
-mysql --host=%the_host% --user=%the_user% --password=%2 --execute="drop schema oscardotnetdb_%1"
-mysql --host=%the_host% --user=%the_user% --password=%2 --execute="create schema oscardotnetdb_%1"
-mysql --host=%the_host% --user=%the_user% --password=%2 --database=oscardotnetdb_%1 <"%USERPROFILE%\my-documents\SANDBOX\vocational\kalipso-infogistics\db-dump\oscardotnetdb.sql"
-mysql --host=%the_host% --user=%the_user% --password=%2 --database=oscardotnetdb_%1 <0-render-safe-for-d-or-x.sql
+mysql --host=%the_host% --user=%the_user% --password=%2 --execute="drop schema %db_instance%"
+mysql --host=%the_host% --user=%the_user% --password=%2 --execute="create schema %db_instance%"
+mysql --host=%the_host% --user=%the_user% --password=%2 --database=%db_instance% <"%USERPROFILE%\my-documents\SANDBOX\vocational\kalipso-infogistics\db-dump\%base_db_name%.sql"
+if "%db_instance%"=="%base_db_name%" goto :END
+mysql --host=%the_host% --user=%the_user% --password=%2 --database=%db_instance% <0-render-safe-for-d-or-x.sql
+:END
+pause
