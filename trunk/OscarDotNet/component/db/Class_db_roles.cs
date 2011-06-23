714c123585ae63b57a02cad68143d24cb8273d03
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2011-06-23 14:06:00 +0000

    Corrected to properly handle case where user's only persistent role is effectively "Member".

--- trunk/OscarDotNet/component/db/Class_db_roles.cs
+++ trunk/OscarDotNet/component/db/Class_db_roles.cs
@@ -184,7 +184,7 @@ namespace Class_db_roles
           {
           var tier_of_name = k.EMPTY;
           Open();
-          tier_of_name = new MySqlCommand("select IFNULL(tier_id,'') as tier_id from role where name = '" + name + "'",connection).ExecuteScalar().ToString();
+          tier_of_name = new MySqlCommand("select IFNULL(tier_id,255) as tier_id from role where name = '" + name + "'",connection).ExecuteScalar().ToString();
           Close();
           return tier_of_name;
           }
