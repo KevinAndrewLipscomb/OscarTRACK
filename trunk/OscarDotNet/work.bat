fbc855647d891b7b72b93e97c2bf2efb3954cd0b
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2011-11-01 11:40:54 +0000

    Generalized the reference to MySQL Workbench.  Removed attachment to Alentus server.

--- trunk/OscarDotNet/work.bat
+++ trunk/OscarDotNet/work.bat
@@ -3,6 +3,5 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\OscarDotNet"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.svn
-start /max explorer ftp://kalipso:@sslws038.alentus.com/ssl/OscarDotNet_x
 start /max OscarDotNet.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 CE\MySQLWorkbench.exe"
+start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
