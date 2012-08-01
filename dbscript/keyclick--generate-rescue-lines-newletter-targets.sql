2c1aaa91cae28fc7f7ccc2f4925892a1ff6e635f
KevinAnLipscomb <kevinandrewlipscomb@vt.edu>
2012-08-01 19:19:28 +0000

    Revised generation of follow-up list to include important generation of recent_donation table, plus cleanup.  Created script to gen rescue lines newsletter targets.

---
+++ dbscript/keyclick--generate-rescue-lines-newletter-targets.sql
@@ -0,0 +1,12 @@
+SELECT IFNULL(resident_base.name,"CURRENT RESIDENT") as name
+, concat(house_num,' ',street.name) as address1
+, city.name as city
+, state.abbreviation as state
+FROM donation
+  join resident_base on (resident_base.id=donation.id)
+  join street on (street.id=resident_base.street_id)
+  join city on (city.id=street.city_id)
+  join state on (state.id=city.state_id)
+WHERE date > DATE_ADD(CURDATE(),INTERVAL -1 YEAR)
+order by amount desc
+LIMIT 1000
