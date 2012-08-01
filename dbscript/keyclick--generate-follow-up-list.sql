4abb7634241ef5580d7073596ad4b792602c6d6e
KevinAnLipscomb <kevinandrewlipscomb@vt.edu>
2012-08-01 18:42:55 +0000

    Creation.

---
+++ dbscript/keyclick--generate-follow-up-list.sql
@@ -0,0 +1,20 @@
+SELECT resident_base.id
+, IFNULL(resident_base.name,"CURRENT RESIDENT")
+, concat(house_num,' ',street.name) as address1
+, city.name as city
+, state.abbreviation as state
+FROM resident_base
+  join street on (street.id=resident_base.street_id)
+  join city on (city.id=street.city_id)
+  join state on (state.id=city.state_id)
+  LEFT JOIN recent_donation on (recent_donation.id=resident_base.id)
+WHERE recent_donation.id IS NULL
+  and agency='KVRS'
+  and 
+    (
+      year_of_last_appeal_to_become_a_donor is null
+    or
+      year_of_last_appeal_to_become_a_donor = "2012"  -- UPDATE THE YEAR
+    )
+  and resident_base.id <> 0
+order by state,city,street.name,house_num
