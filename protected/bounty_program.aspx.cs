485a210f22d82f0d61e2511344707440463644b6
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2012-12-16 21:02:32 +0000

    Added bounty program.

---
+++ protected/bounty_program.aspx.cs
@@ -0,0 +1,71 @@
+// Derived from template~protected~nonlanding.aspx.cs~template
+
+using kix;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace bounty_program
+  {
+  public partial class TWebForm_bounty_program: ki_web_ui.page_class
+    {
+
+    private struct p_type
+      {
+      }
+
+    private p_type p;
+
+    // / <summary>
+    // / Required method for Designer support -- do not modify
+    // / the contents of this method with the code editor.
+    // / </summary>
+    private void InitializeComponent()
+      {
+      //this.Load += this.Page_Load;
+      this.PreRender += this.TWebForm_bounty_program_PreRender;
+      }
+
+    protected void Page_Load(object sender, System.EventArgs e)
+      {
+      if (!IsPostBack)
+        {
+        Title = Server.HtmlEncode(ConfigurationManager.AppSettings["application_name"]) + " - bounty_program";
+        }
+      }
+
+    protected override void OnInit(EventArgs e)
+      {
+      // Required for Designer support
+      InitializeComponent();
+      base.OnInit(e);
+      var nature_of_visit = NatureOfVisitUnlimited(InstanceId() + ".p");
+      if (nature_of_visit == nature_of_visit_type.VISIT_INITIAL)
+        {
+        //
+        // Initialize p.~ objects here.
+        //
+        }
+      else if (nature_of_visit == nature_of_visit_type.VISIT_POSTBACK_STANDARD)
+        {
+        p = (p_type)(Session[InstanceId() + ".p"]);
+        }
+      //
+      // ScriptManager.GetCurrent(Page).EnablePartialRendering = false;
+      //
+      }
+
+    private void TWebForm_bounty_program_PreRender(object sender, System.EventArgs e)
+      {
+      SessionSet(InstanceId() + ".p", p);
+      }
+
+    } // end TWebForm_bounty_program
+
+  }
