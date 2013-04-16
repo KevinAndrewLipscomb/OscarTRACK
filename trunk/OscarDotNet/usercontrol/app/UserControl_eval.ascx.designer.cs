2dcf562e1c890ddca7cae7c365114c0519ce6819
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2013-04-16 22:18:20 +0000

    Revised to prevent evaluator from ever evaluating self.

--- trunk/OscarDotNet/usercontrol/app/UserControl_eval.ascx.designer.cs
+++ trunk/OscarDotNet/usercontrol/app/UserControl_eval.ascx.designer.cs
@@ -391,6 +391,15 @@ namespace UserControl_eval {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator_aic_member_id;
         
         /// <summary>
+        /// CompareValidator_aic control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator CompareValidator_aic;
+        
+        /// <summary>
         /// Label_alt_aic_reason control.
         /// </summary>
         /// <remarks>
