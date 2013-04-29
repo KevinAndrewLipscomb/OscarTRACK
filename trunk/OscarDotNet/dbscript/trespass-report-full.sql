643b182de5883e1b176c4f4de6000ba8d28edc54
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2013-04-29 20:04:00 +0000

    Revised to eliminate duplicate indications.

--- trunk/OscarDotNet/dbscript/trespass-report-full.sql
+++ trunk/OscarDotNet/dbscript/trespass-report-full.sql
@@ -1,7 +1,7 @@
 select "!ERR!" as status
 , x.house_num
 , street.name
-, CONCAT("claimed by ",GROUP_CONCAT(x.agency order by x.agency separator " and ")) as agencies
+, CONCAT("claimed by ",GROUP_CONCAT(DISTINCT x.agency order by x.agency separator " and ")) as agencies
 from resident_base x
   join resident_base y on (y.agency<>x.agency and y.street_id=x.street_id and y.house_num=x.house_num)
   join street on (street.id=x.street_id)
