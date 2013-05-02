d47f044f2e53a1ff979e36ed8202c65b15b09240
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2013-05-02 21:12:07 +0000

    Creation.

---
+++ trunk/OscarDotNet/dbscript/keyclick--multi-squad-territory-map-data.sql
@@ -0,0 +1,20 @@
+select agency,address,city,state from 
+  (
+  select agency
+  , concat(house_num," ",street.name) as address
+  , city.name as city
+  , state.abbreviation as state
+  , house_num
+  , street.name as street
+  from resident_base
+    join street on (street.id=resident_base.street_id)
+    join city on (city.id=street.city_id)
+    join state on (state.id=city.state_id)
+  where resident_base.id > 0
+    and street.name <> "PO BOX"
+    and city.name = "VIRGINIA BEACH"
+    and agency in ("CBVRS","PACHVRS","KVRS","VBVRS")
+  order by RAND()
+  limit 10000
+  ) as sample
+order by state,city,street,house_num,agency
