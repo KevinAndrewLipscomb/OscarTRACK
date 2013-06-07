bfbc8001a4fbeb80bd8c1b75ff95713350199fec
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2013-06-07 22:39:10 +0000

    Creation.

---
+++ trunk/OscarDotNet/dbscript/2013-06-07-B--add-vehicle-quarters-recs-for-mrt.sql
@@ -0,0 +1,6 @@
+START TRANSACTION
+;
+insert ignore vehicle_quarters (medium_designator,long_designator,pecking_order) values
+("RB trailer standby lot","Rescue boat trailer standby lot",40100)
+;
+COMMIT
