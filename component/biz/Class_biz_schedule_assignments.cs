849983a68817fad9f63c42313a0f577538af71d1
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2013-06-17 18:41:38 +0000

    In PublishPendingNotifications, added debug call to k.SmtpMailSend.

--- component/biz/Class_biz_schedule_assignments.cs
+++ component/biz/Class_biz_schedule_assignments.cs
@@ -760,6 +760,15 @@ namespace Class_biz_schedule_assignments
       var num_targets = member_id_q.Count;
       var member_id = k.EMPTY;
       var other_agency_ids = k.EMPTY;
+//
+k.SmtpMailSend
+  (
+  from:ConfigurationManager.AppSettings["sender_email_address"],
+  to:"debug@frompaper2web.com",
+  subject:biz_members.LastNameOfMemberId(publisher_member_id) + k.SPACE + " published month " + relative_month.val.ToString() + " virgin " + be_virgin_watchbill.ToString() + " preview " + be_limited_preview.ToString(),
+  message_string:k.EMPTY
+  );
+//
       for (var i = new k.subtype<int>(0,num_targets); i.val < num_targets; i.val++)
         {
         member_id = (member_id_q.Dequeue() as string);
