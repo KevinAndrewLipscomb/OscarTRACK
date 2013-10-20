d4e03b6708f1221513f4e850ace84954d334bf8d
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2013-10-20 20:52:20 +0000

    Creation.

---
+++ dbscript/keyclick--export-for-fund-drive--nondonor-sample.sql
@@ -0,0 +1,22 @@
+select resident_base.id as id
+, IFNULL(resident_base.name,"TO OUR FRIENDS AT") as name
+, IF(
+      street.name = "PO BOX"
+    ,
+      concat(street.name,' ',house_num)
+    ,  
+      concat(house_num,' ',street.name)
+    )
+    as address
+, city.name as city
+, state.abbreviation as state
+from resident_base
+  join street on (street.id=resident_base.street_id)
+  join city on (city.id=street.city_id)
+  join state on (state.id=city.state_id)
+where resident_base.id > 0
+  and agency = "KVRS"
+  and resident_base.id not in (select id from donation)
+  and city.name = "VIRGINIA BEACH"
+order by state.abbreviation,city.name,street.name,house_num,rand()
+limit 10384
