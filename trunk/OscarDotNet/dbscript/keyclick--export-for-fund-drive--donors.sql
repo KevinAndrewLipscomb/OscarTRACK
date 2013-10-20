65a214bf8cd207b2143652f79e0196eaf364ea99
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2013-10-20 20:52:20 +0000

    Creation.

---
+++ trunk/OscarDotNet/dbscript/keyclick--export-for-fund-drive--donors.sql
@@ -0,0 +1,22 @@
+select resident_base.id as id
+, IFNULL(resident_base.name,"TO OUR FRIENDS AT") as name
+, IF(
+      street.name = "PO BOX"
+    ,
+      concat(street.name,' ',house_num)
+    ,  
+      concat(house_num,' ',street.name)
+    )
+    as address
+, city.name as city
+, state.abbreviation as state
+from resident_base
+  join street on (street.id=resident_base.street_id)
+  join city on (city.id=street.city_id)
+  join state on (state.id=city.state_id)
+  join donation on (donation.id=resident_base.id)
+where resident_base.id > 0
+  and agency = "KVRS"
+group by donation.id
+  having sum(amount) > 0
+order by state.abbreviation,city.name,street.name,house_num
