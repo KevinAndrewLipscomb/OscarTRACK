ac359ecf0f8dfd969aeec65aa0753a684ba3f1d8
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2014-02-22 00:28:19 +0000

    Revised so archival end-of-month watchbills will have depth set to All.

--- trunk/OscarDotNet/noninteractive/report_archival_end_of_month_watchbill.aspx.cs
+++ trunk/OscarDotNet/noninteractive/report_archival_end_of_month_watchbill.aspx.cs
@@ -64,7 +64,7 @@ namespace report_archival_end_of_month_watchbill
         p.relative_month = new k.subtype<int>(0,1);
         //
         Session.Add("mode:report", k.EMPTY);
-        Session.Add("mode:report/archival_end_of_month-watchbill-noninteractive", k.EMPTY);
+        Session.Add("mode:report/archival-end-of-month-watchbill-noninteractive", k.EMPTY);
         //
         var UserControl_schedule_proposal_control = ((LoadControl("~/usercontrol/app/UserControl_schedule_proposal.ascx") as TWebUserControl_schedule_proposal));
         PlaceHolder_schedule_proposal.Controls.Add(UserControl_schedule_proposal_control);
