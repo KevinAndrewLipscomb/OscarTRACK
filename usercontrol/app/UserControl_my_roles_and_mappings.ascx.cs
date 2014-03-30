3b2ec10b72649c6572e5f182213d211f1a8f9fe7
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2014-03-30 00:08:32 +0000

    Added my_roles_and_mappings feature.

---
+++ usercontrol/app/UserControl_my_roles_and_mappings.ascx.cs
@@ -0,0 +1,106 @@
+// Derived from KiAspdotnetFramework/UserControl/app/UserControl~template~datagrid~sortable.ascx.cs
+
+using Class_biz_members;
+using Class_biz_notifications;
+using Class_biz_user;
+using kix;
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+namespace UserControl_my_roles_and_mappings
+  {
+
+  public partial class TWebUserControl_my_roles_and_mappings: ki_web_ui.usercontrol_class
+    {
+
+    private struct p_type
+      {
+      public bool be_interactive;
+      public bool be_loaded;
+      public TClass_biz_members biz_members;
+      public TClass_biz_notifications biz_notifications;
+      public TClass_biz_user biz_user;
+      }
+
+    private p_type p;
+
+    protected void Page_Load(object sender, System.EventArgs e)
+      {
+      if (!p.be_loaded)
+        {
+        BulletedList_role.DataSource = p.biz_user.Roles();
+        BulletedList_role.DataBind();
+        TableRow_no_roles.Visible = (BulletedList_role.Items.Count == 0);
+        //
+        BulletedList_privilege.DataSource = (Session["privilege_array"] as string[]);
+        BulletedList_privilege.DataBind();
+        TableRow_no_privileges.Visible = (BulletedList_privilege.Items.Count == 0);
+        //
+        p.biz_notifications.BindDirectToListControlForMember(p.biz_members.IdOfUserId(p.biz_user.IdNum()),BulletedList_notification);
+        TableRow_no_notifications.Visible = (BulletedList_notification.Items.Count == 0);
+        //
+        p.be_loaded = true;
+        }
+      }
+
+    protected override void OnInit(System.EventArgs e)
+      {
+      // Required for Designer support
+      InitializeComponent();
+      base.OnInit(e);
+      var instance_id = InstanceId();
+      if (Session[instance_id + ".p"] != null)
+        {
+        p = (p_type)(Session[instance_id + ".p"]);
+        p.be_loaded = IsPostBack;  // This test is sufficient if this control is being used statically on its page.
+        //
+        // If this control is being used dynamically under one or more parent binder(s), it must ascertain which instance it is, and whether or not that instance's parent binder
+        // had it loaded already.
+        //
+        if (instance_id == "ASP.protected_overview_aspx.UserControl_member_binder_config_binder_roles_and_matrices_binder_my_roles_and_mappings")
+          {
+          p.be_loaded &= ((Session["UserControl_member_binder_UserControl_config_binder_UserControl_roles_and_matrices_binder_PlaceHolder_content"] as string) == "UserControl_my_roles_and_mappings");
+          }
+//      else if (instance_id == "ASP.~_aspx.UserControl_~_binder_my_roles_and_mappings")
+//        {
+//        p.be_loaded &= ((Session["UserControl_~_binder_PlaceHolder_content"] as string) == "UserControl_my_roles_and_mappings");
+//        }
+        }
+      else
+        {
+        p.biz_members = new TClass_biz_members();
+        p.biz_notifications = new TClass_biz_notifications();
+        p.biz_user = new TClass_biz_user();
+        //
+        p.be_interactive = (Session["mode:report"] == null);
+        p.be_loaded = false;
+        }
+      }
+
+    // / <summary>
+    // / Required method for Designer support -- do not modify
+    // / the contents of this method with the code editor.
+    // / </summary>
+    private void InitializeComponent()
+      {
+      PreRender += TWebUserControl_my_roles_and_mappings_PreRender;
+      }
+
+    private void TWebUserControl_my_roles_and_mappings_PreRender(object sender, System.EventArgs e)
+      {
+      SessionSet(InstanceId() + ".p", p);
+      }
+
+    public TWebUserControl_my_roles_and_mappings Fresh()
+      {
+      Session.Remove(InstanceId() + ".p");
+      return this;
+      }
+
+    } // end TWebUserControl_my_roles_and_mappings
+
+  }
--- usercontrol/app/UserControl_roles_and_matrices_binder.ascx
+++ usercontrol/app/UserControl_roles_and_matrices_binder.ascx
@@ -6,6 +6,7 @@
       <tr>
         <td>
           <cc1:TabContainer id="TabContainer_control" runat="server" autopostback="True" cssclass="ajax__tab_ki">
+            <cc1:TabPanel runat="server" headertext="Mine" id="TabPanel_my_roles_and_mappings"></cc1:TabPanel>
             <cc1:TabPanel runat="server" headertext="Roles" id="TabPanel_roles"></cc1:TabPanel>
             <cc1:TabPanel runat="server" headertext="Role-Member mappings" id="TabPanel_role_member_mappings"></cc1:TabPanel>
             <cc1:TabPanel runat="server" headertext="Role-Privilege mappings" id="TabPanel_role_privilege_mappings"></cc1:TabPanel>
@@ -16,7 +17,7 @@
       <tr>
         <td style="BORDER-RIGHT: silver thin solid; PADDING-RIGHT: 10px; PADDING-LEFT: 10px; PADDING-BOTTOM: 10px; BORDER-LEFT: silver thin solid; PADDING-TOP: 10px; BORDER-BOTTOM: silver thin solid">
           <p>&nbsp;</p>
-          <p>Have you read the <a href="../doc/roles.aspx" target="_blank">Important Information About Roles</a>?&nbsp; <small>(opens new window)</small>
+          <p>Have you read the <a href="~/doc/roles.aspx" target="_blank">Important Information About Roles</a>?&nbsp; <small>(opens new window)</small>
           </p>
           <ASP:PlaceHolder id="PlaceHolder_content" runat="server"></ASP:PlaceHolder>
         </td>
