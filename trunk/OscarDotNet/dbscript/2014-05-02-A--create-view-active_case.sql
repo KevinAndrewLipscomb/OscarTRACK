965ccb942fbb4b2bb0c64344101c3f79acc94e29
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2014-05-03 19:28:54 +0000

    Corrected the included query.  Next step is to reduce this to a utility query instead of a view definition.

--- trunk/OscarDotNet/dbscript/2014-05-02-A--create-view-active_case.sql
+++ trunk/OscarDotNet/dbscript/2014-05-02-A--create-view-active_case.sql
@@ -1,24 +1,24 @@
-START TRANSACTION
-;
-drop view if exists active_case
-;
-create view active_case as
-SELECT id
-, incident_date as date
-, max(incident_num) as case_num
-, incident_address as address
-, call_sign as unit
-, DATE_FORMAT(time_initialized,'%H:%i') as init
-, DATE_FORMAT(time_of_alarm,'%H:%i') as alarm
-, DATE_FORMAT(time_enroute,'%H:%i') as enrt
-, DATE_FORMAT(time_on_scene,'%H:%i') as onsc
-, DATE_FORMAT(time_transporting,'%H:%i') as tprt
-, DATE_FORMAT(time_at_hospital,'%H:%i') as hosp
-, DATE_FORMAT(time_available,'%H:%i') as avbl
-FROM cad_record
+select id
+, incident_date
+, incident_num
+, incident_address
+, reduced.call_sign
+, DATE_FORMAT(time_initialized,'%H:%i') as time_initialized
+, DATE_FORMAT(time_of_alarm,'%H:%i') as time_of_alarm
+, DATE_FORMAT(time_enroute,'%H:%i') as time_enroute
+, DATE_FORMAT(time_on_scene,'%H:%i') as time_onscene
+, DATE_FORMAT(time_transporting,'%H:%i') as time_transporting
+, DATE_FORMAT(time_at_hospital,'%H:%i') as time_at_hospital
+, DATE_FORMAT(time_available,'%H:%i') as time_available
+from cad_record detail inner join
+  (
+  SELECT call_sign
+  , max(incident_num) as max_incident_num
+  FROM cad_record
+  where call_sign not in ('ARSN','EYES','FAST','FIGP','MISC')
+  group by call_sign
+  )
+  as reduced
+    on (reduced.call_sign=detail.call_sign and reduced.max_incident_num=detail.incident_num)
 where time_available is null
-  and call_sign not in ('ARSN','EYES','FAST','FIGP','MISC')
-group by call_sign
 order by incident_num desc, id desc
-;
-COMMIT
