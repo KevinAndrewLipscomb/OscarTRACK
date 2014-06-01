ad4889f4e940f3111f1c9cb0ee99131e40a497f4
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2014-06-01 00:12:09 +0000

    Creation.

---
+++ trunk/OscarDotNet/dbscript/2014-05-31-A--convert-thalia-from-r07-to-r15.sql
@@ -0,0 +1,36 @@
+START TRANSACTION
+;
+update agency
+set long_designator = "Town Center"
+, be_ems_post = FALSE
+, door_code = null
+, address = '4817 COLUMBUS ST'
+where short_designator = 'R07'
+;
+update agency
+set long_designator = "Thalia"
+, keyclick_enumerator = null
+, oscar_classic_enumerator = null
+, be_ems_post = TRUE
+, address = '407 THALIA RD'
+where short_designator = 'R15'
+;
+insert ignore agency
+set id = 315
+, short_designator = 'Z15'
+, medium_designator = 'Zone 15'
+, long_designator = 'Zone 15'
+, be_ems_post = TRUE
+, ems_system_id = 1
+;
+update schedule_assignment
+set post_id = 15
+where nominal_day > '2014-05-31'
+  and post_id = 7
+;
+update schedule_assignment
+set post_id = 315
+where nominal_day > '2014-05-31'
+  and post_id = 307
+;
+COMMIT
