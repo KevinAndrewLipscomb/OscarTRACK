2d14c4a93d234b5263557eab03eba6fcd35de29a
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2014-06-28 19:36:08 +0000

    Revised to not supress when user is a supervisor.

--- trunk/OscarDotNet/usercontrol/app/UserControl_schedule_assignment_assistant_holdouts.ascx.cs
+++ trunk/OscarDotNet/usercontrol/app/UserControl_schedule_assignment_assistant_holdouts.ascx.cs
@@ -162,7 +162,11 @@ namespace UserControl_schedule_assignment_assistant_holdouts
         }
       else if
         (
-          (k.Has(Session["privilege_array"] as string[],"edit-schedule") && (p.agency_filter == own_agency || p.agency_filter == k.EMPTY))
+          (
+            (k.Has(Session["privilege_array"] as string[],"edit-schedule") || p.biz_schedule_assignments.BeOkToEditScheduleTierDepartmentOnly(privilege_array:Session["privilege_array"] as string[]))
+          &&
+            (p.agency_filter == own_agency || p.agency_filter == k.EMPTY)
+          )
         ||
           p.biz_privileges.HasForSpecialAgency(member_id:p.biz_members.IdOfUserId(p.biz_user.IdNum()),privilege_name:"edit-schedule",agency_id:p.agency_filter)
         )
