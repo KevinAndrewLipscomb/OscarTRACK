ce6a67dd45cce6d44b289a958dbe084ded747dbc
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2014-07-26 23:17:15 +0000

    Moved field_situation.sql from OscarDotNet to OSCALERT.  In work.bat, generalized the invocation of MySQL Workbench.

--- trunk/OscarDotNet/dbscript/field_situation.sql
+++
@@ -1,8 +0,0 @@
-SELECT case_num
-, time_initialized
-, address
-, assignment
-, description
-FROM field_situation
-  join field_situation_impression on (field_situation_impression.id=field_situation.impression_id)
-order by case_num desc, field_situation.id desc
