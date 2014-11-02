7acb3a5d3e7caaa59b51f49783cf94d0555b13c1
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2014-11-02 16:47:46 +0000

    Alpha work on PayPal IPN listener.

---
+++ component/biz/Class_biz_paypal_ipn_listener.cs
@@ -0,0 +1,82 @@
+using kix;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Configuration;
+
+namespace Class_biz_paypal_ipn_listener
+  {
+
+  public class TClass_biz_paypal_ipn_listener
+    {
+
+    internal TClass_biz_paypal_ipn_listener() : base()
+      {
+      }
+
+    internal void ProcessNotification
+      (
+      string message,
+      string payer_email,
+      string payment_status,
+      string receiver_email,
+      string mc_gross
+      )
+      {
+      //
+      // Prepend PayPal validation argument.
+      //
+      var readback = "cmd=_notify-validate&" + message;
+      //
+      // create an HttpRequest channel to perform handshake with PayPal
+      //
+      var http_web_request = (HttpWebRequest)WebRequest.Create("https://www.paypal.com/cgi-bin/webscr");
+      http_web_request.Method = "POST";
+      http_web_request.ContentType = "application/x-www-form-urlencoded";
+      http_web_request.ContentLength = readback.Length;
+      //
+      // send data back to PayPal to request verification
+      //
+      var stream_writer = new StreamWriter(http_web_request.GetRequestStream(), Encoding.ASCII);
+      stream_writer.Write(readback);
+      stream_writer.Close();
+      //
+      // receive response from PayPal
+      //
+      var stream_reader = new StreamReader(http_web_request.GetResponse().GetResponseStream());
+      var response = stream_reader.ReadToEnd();
+      stream_reader.Close();
+      //
+//
+k.SmtpMailSend
+  (
+  from:ConfigurationManager.AppSettings["sender_email_address"],
+  to:ConfigurationManager.AppSettings["sender_email_address"],
+  subject:"PayPal IPN",
+  message_string:message
+  );
+//
+      if (response.Equals("VERIFIED"))
+        {
+        //
+        // paypal has verified the data, it is safe for us to perform processing now
+        //
+        if (payment_status.Equals("Completed"))
+          {
+          //
+          // if the seller email is us (we don't want anyone else getting our payment!)
+          //
+          if (receiver_email.ToLower().Equals("my.paypal.seller.account@mycompany.com"))
+            {
+            //
+            // complete order processing
+            // E.g. arrange for despatch of products, mark order as complete, send emails out,
+            //
+            }
+          }
+        }       
+      }
+
+    } // end TClass_biz_paypal_ipn_listener
+
+  }
---
+++ paypal_ipn_listener.aspx
@@ -0,0 +1,11 @@
+<%@ Page Language="c#" Debug="true" CodeBehind="paypal_ipn_listener.aspx.cs" AutoEventWireup="True" Inherits="paypal_ipn_listener.TWebForm_paypal_ipn_listener" validateRequest="false"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body bgcolor="white">
+    <form runat="server">
+    </form>
+  </body>
+</html>
