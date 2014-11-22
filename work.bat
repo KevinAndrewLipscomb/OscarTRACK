86f1fc35018bea98f0808d1e3004061e8466ae6a
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2014-11-22 22:50:44 +0000

    Revised to work on both Win32 and Win64.

--- work.bat
+++ work.bat
@@ -4,4 +4,4 @@ REM
 cd "C:\Inetpub\wwwroot\OscarDotNet"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.svn
 start /max OscarDotNet.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
+IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
