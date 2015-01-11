8f47d92b4cb284f14ca5e497afa029513f48c9f9
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2015-01-11 23:19:14 +0000

    Added schedule_assignment_log_assignment_id constraint with 'on delete cascade' to achieve an auto-purge.

--- trunk/OscarDotNet/dbscript/2015-01-10-A--create-schedule_assignment_log.sql
+++ trunk/OscarDotNet/dbscript/2015-01-10-A--create-schedule_assignment_log.sql
@@ -20,6 +20,8 @@ CREATE TABLE `schedule_assignment_log`
   ,
     KEY `actor_member_id` (`actor_member_id`)
   ,
+    CONSTRAINT `schedule_assignment_log_assignment_id` FOREIGN KEY (`assignment_id`) REFERENCES `schedule_assignment` (`id`) ON DELETE CASCADE
+  ,
     CONSTRAINT `schedule_assignment_log_actor_member_id` FOREIGN KEY (`actor_member_id`) REFERENCES `member` (`id`)
   )
   ENGINE=InnoDB
