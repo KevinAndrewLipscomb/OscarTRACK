2ef338a3ed49378733f9b01262be2d07464094e8
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2015-05-12 14:32:42 +0000

    Beta work on Active Case Board.

--- component/biz/Class_biz_residents.cs
+++ component/biz/Class_biz_residents.cs
@@ -109,6 +109,13 @@ namespace Class_biz_residents
         scene_visits = Regex.Replace
           (
           input:scene_visits,
+          pattern:"(?<prefix> )CS$|(?<prefix> )CS(?<suffix> )",
+          replacement:"${prefix}CLOSE${suffix}",
+          options:RegexOptions.Multiline
+          );
+        scene_visits = Regex.Replace
+          (
+          input:scene_visits,
           pattern:"(?<prefix> )CW$|(?<prefix> )CW(?<suffix> )",
           replacement:"${prefix}CAUSEWAY${suffix}",
           options:RegexOptions.Multiline
