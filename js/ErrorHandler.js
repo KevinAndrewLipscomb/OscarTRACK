66982395527f282518fcf2922b95f35fe7170439
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2015-07-14 21:20:23 +0000

    Revised to convert certain known error messages into useful instructions to the user.

--- js/ErrorHandler.js
+++ js/ErrorHandler.js
@@ -2,7 +2,16 @@ function EndRequestHandler(sender, args)
   {
   if (args.get_error() != undefined)
     {
-    alert(args.get_error().message.replace('Sys.WebForms.PageRequestManagerServerErrorException: ',""));
+    var outgoing_error_message = incoming_error_message = args.get_error().message;
+    if(
+        (incoming_error_message == "Sys.WebForms.PageRequestManagerParserErrorException: The message received from the server could not be parsed.")
+      ||
+        (incoming_error_message.indexOf("Sys.WebForms.PageRequestManagerServerErrorException: ") == 0)
+      )
+      {
+      outgoing_error_message = "The server sent something unexpected.  If the web application continues to behave abnormally, try logging out, then logging back in again.\n\nSorry for the inconvenience.";
+      }
+    alert(outgoing_error_message);
     args.set_errorHandled(true);
     }
   }
