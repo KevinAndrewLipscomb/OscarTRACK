89050c5215bc9e2c915fc422a10dfa5b67dc2fe8
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2015-08-19 21:47:47 +0000

    Added access to SMS gateways configuration.

--- usercontrol/app/UserControl_business_objects_binder.ascx.designer.cs
+++ usercontrol/app/UserControl_business_objects_binder.ascx.designer.cs
@@ -85,6 +85,15 @@ namespace UserControl_business_objects_binder {
         protected global::AjaxControlToolkit.TabPanel TabPanel_eval_objects;
         
         /// <summary>
+        /// TabPanel_sms_gateways control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.TabPanel TabPanel_sms_gateways;
+        
+        /// <summary>
         /// TabPanel_cad_objects control.
         /// </summary>
         /// <remarks>
