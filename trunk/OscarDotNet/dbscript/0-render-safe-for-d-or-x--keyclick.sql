4846457518c9c46d27de02ef952672f03cbe7ac1
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2016-01-20 20:41:26 +0000

    Revised to treat collected donor email addresses.

--- trunk/OscarDotNet/dbscript/0-render-safe-for-d-or-x--keyclick.sql
+++ trunk/OscarDotNet/dbscript/0-render-safe-for-d-or-x--keyclick.sql
@@ -1,6 +1,9 @@
-START TRANSACTION;
-
+START TRANSACTION
+;
 update user
-set email_addr = concat(replace(replace(replace(email_addr,"@",""),".","")," ",""),"@frompaper2web.com");
-
+set email_addr = concat(replace(replace(replace(email_addr,"@",""),".","")," ",""),"@frompaper2web.com")
+;
+update web_donor
+set email_address = concat(replace(email_address,"@","."),"@frompaper2web.com")
+;
 COMMIT
