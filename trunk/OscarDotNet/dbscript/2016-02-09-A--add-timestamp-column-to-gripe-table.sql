7b25f7c7537484b4ebac39261c0e4bcaf6a1bdfa
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2016-02-10 20:25:28 +0000

    Revised to reflect what actually had to be run into production.

--- trunk/OscarDotNet/dbscript/2016-02-09-A--add-timestamp-column-to-gripe-table.sql
+++ trunk/OscarDotNet/dbscript/2016-02-09-A--add-timestamp-column-to-gripe-table.sql
@@ -4,7 +4,7 @@ ALTER TABLE `gripe`
   ADD COLUMN `last_entry_datetime` DATETIME NULL ON UPDATE CURRENT_TIMESTAMP
 ;
 update gripe
-set last_entry_datetime = CONCAT(DATE(@reduction_3 := LEFT(SUBSTRING(@reduction_2 := SUBSTRING(@reduction_1 := REPLACE(REPLACE(SUBSTRING(description,LENGTH(description) - LOCATE(' ==v==',REVERSE(description)) + 2),' JR. ',' '),' SR ',' '),LOCATE(' ',@reduction_1) + 1),LOCATE(' ',@reduction_2) + 1),19)),' ',REPLACE(TIME(@reduction_3),'.000000',''))
+set last_entry_datetime = CONCAT(DATE(@reduction_3 := LEFT(SUBSTRING(@reduction_2 := SUBSTRING(@reduction_1 := REPLACE(REPLACE(REPLACE(SUBSTRING(description,LENGTH(description) - LOCATE(' ==v==',REVERSE(description)) + 2),' JR. ',' '),' SR ',' '),' JR ',' '),LOCATE(' ',@reduction_1) + 1),LOCATE(' ',@reduction_2) + 1),19)),' ',REPLACE(TIME(@reduction_3),'.000000',''))
 ;
 ALTER TABLE `gripe` 
   CHANGE COLUMN `last_entry_datetime` `last_entry_datetime` DATETIME NOT NULL
