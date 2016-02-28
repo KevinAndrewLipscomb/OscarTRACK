07d151106d4fc36b52b997ce2091933260c0031f
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2016-02-28 15:27:04 +0000

    Added meta-refresh code to prevent the timeout page itself from timing out if left idle in a user's browser for longer than the session timeout period.

--- timeout.aspx.designer.cs
+++ timeout.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace timeout {
     public partial class TWebForm_timeout {
         
         /// <summary>
+        /// Meta_control control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlMeta Meta_control;
+        
+        /// <summary>
         /// UserControl_precontent control.
         /// </summary>
         /// <remarks>
