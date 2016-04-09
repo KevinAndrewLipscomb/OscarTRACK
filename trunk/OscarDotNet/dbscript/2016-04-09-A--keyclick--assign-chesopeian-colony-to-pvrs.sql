d5d1ff287be54cc9a1fde69d0a0f2a9942f66691
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2016-04-09 20:25:15 +0000

    Creation.

---
+++ trunk/OscarDotNet/dbscript/2016-04-09-A--keyclick--assign-chesopeian-colony-to-pvrs.sql
@@ -0,0 +1,30 @@
+START TRANSACTION
+;
+update resident_base
+  join street on (street.id=resident_base.street_id)
+set agency = 'PVRS'
+where street.name like 'chesopeian %'
+  or street.name like 'dix inlet %'
+  or street.name like 'cattayle %'
+  or street.name like 'west point %'
+  or street.name like 'shepherds q%'
+  or street.name = 'pine ridge ln'
+  or street.name like 'sandy valley %'
+  or street.name like 'mulberry grove %'
+  or street.name like 'timber neck %'
+  or street.name like 'chester forest %'
+  or street.name like '_ kings %'
+  or street.name like 'apasus %'
+  or street.name like 'queen anne %'
+  or street.name like 'tarkill %'
+  or street.name = 'crawford pl'
+  or street.name like 'colony pines %'
+  or street.name like 'little pine %'
+  or
+    (
+      street.name = 'virginia beach blvd'
+    and
+      (house_num like '26__%' or house_num like '27__%' or house_num like '28__%')
+    )
+;
+COMMIT
