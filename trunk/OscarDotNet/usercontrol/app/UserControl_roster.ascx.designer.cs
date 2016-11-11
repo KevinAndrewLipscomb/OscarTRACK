d7deee9d4e7fd7cbc60364a9090e4bc2f375e322
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2016-11-11 23:41:11 +0000

    Added feature to allow privileged users to download this control's QuickMessage target list.

--- trunk/OscarDotNet/usercontrol/app/UserControl_roster.ascx.designer.cs
+++ trunk/OscarDotNet/usercontrol/app/UserControl_roster.ascx.designer.cs
@@ -317,5 +317,14 @@ namespace UserControl_roster {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label_distribution_list;
+        
+        /// <summary>
+        /// Button_download_distribution_list control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button_download_distribution_list;
     }
 }
