4f99a30183d2fcffc3d47b6c2112380357347dac
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2016-12-22 17:03:49 +0000

    Revised KEYclick import scripts to properly match addresses with single-digit house numbers.  Added user email address to donation_ack_letter.

--- usercontrol/app/UserControl_donation_ack_letter.ascx.designer.cs
+++ usercontrol/app/UserControl_donation_ack_letter.ascx.designer.cs
@@ -92,5 +92,14 @@ namespace UserControl_donation_ack_letter {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Literal Literal_member_title;
+        
+        /// <summary>
+        /// Label_member_email_address control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label_member_email_address;
     }
 }
