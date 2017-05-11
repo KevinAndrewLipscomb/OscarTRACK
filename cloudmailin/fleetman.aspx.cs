a0bc1b85d14507858e473ef1e14aae5ba1d17131
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2017-05-11 21:38:23 +0000

    Alpha work on cloudmailin/fleetman feature.

---
+++ cloudmailin/fleetman.aspx.cs
@@ -0,0 +1,38 @@
+using Class_biz_fleetman;
+using System;
+
+namespace fleetman
+  {
+
+  public partial class TWebForm_fleetman: System.Web.UI.Page
+    {
+
+    // / <summary>
+    // / Required method for Designer support -- do not modify
+    // / the contents of this method with the code editor.
+    // / </summary>
+    private void InitializeComponent()
+      {
+      }
+
+    protected void Page_Load(object sender, System.EventArgs e)
+      {
+      new TClass_biz_fleetman().ProcessCloudmailinRequest
+        (
+        x_to_header:Request["x_to_header"],
+        subject:Request["subject"],
+        plain:Request["plain"],
+        message:Request["message"]
+        );
+      }
+
+    protected override void OnInit(EventArgs e)
+      {
+      // Required for Designer support
+      InitializeComponent();
+      base.OnInit(e);
+      }
+
+    } // end TWebForm_fleetman
+
+  }
