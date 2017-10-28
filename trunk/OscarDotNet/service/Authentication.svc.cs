0c887f0b51a56f79b763ad417230dceeb7ec15c1
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2017-10-28 16:27:44 +0000

    Alpha work on service/Authentication.svc.

--- trunk/OscarDotNet/service/Authentication.svc.cs
+++ trunk/OscarDotNet/service/Authentication.svc.cs
@@ -4,7 +4,7 @@ namespace OscarDotNet.service
   {
   // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "authentication" in code, svc and config file together.
   // NOTE: In order to launch WCF Test Client for testing this service, please select authentication.svc or authentication.svc.cs at the Solution Explorer and start debugging.
-  public class authentication : Iauthentication
+  public class Authentication : IAuthentication
     {
 
     private TClass_biz_users biz_users = new TClass_biz_users();
