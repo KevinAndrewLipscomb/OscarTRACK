5eb5d6c2f98f2b50d084081565521d2d748b78c9
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2017-10-28 16:27:44 +0000

    Alpha work on service/Authentication.svc.

--- service/IAuthentication.cs
+++ service/IAuthentication.cs
@@ -4,7 +4,7 @@ namespace OscarDotNet.service
   {
   // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "Iauthentication" in both code and config file together.
   [ServiceContract]
-  public interface Iauthentication
+  public interface IAuthentication
     {
     [OperationContract]
     bool BeAuthorized
