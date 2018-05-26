922ce34b48d343628b69133f879660a633483303
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2018-05-26 22:01:00 +0000

    Added where condition to prevent pulling email addresses belonging to love letter respondents etc.

--- dbscript/keyclick--get-targets-for-online-newsletter.sql
+++ dbscript/keyclick--get-targets-for-online-newsletter.sql
@@ -2,6 +2,7 @@ select *
 from web_donor
   join resident_base on (resident_base.id=web_donor.resident_id)
 where agency = 'KVRS'
+  and resident_id > 0
   and not be_unsubscribed_from_marketing
 order by email_address
 --
