e9c4b524caab2b24ad9e14a8420852ec9d18e3bf
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2018-06-17 13:44:47 +0000

    Revised to target .Net Framework 4.6.1 to support security-related requirements.  Created service/FleetStatus.

---
+++ service/IFleetStatus.cs
@@ -0,0 +1,22 @@
+﻿using Class_db_vehicles;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace OscarDotNet.service
+  {
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFleetStatus" in both code and config file together.
+  [ServiceContract]
+  public interface IFleetStatus
+    {
+    [OperationContract]
+    List<vehicle_summary> Items
+      (
+      string sort_order,
+      bool be_sort_order_ascending,
+      string agency_filter,
+      string vehicle_kind_filter,
+      bool be_four_or_all_wheel_drive_filter,
+      string quarters_filter
+      );
+    }
+  }
