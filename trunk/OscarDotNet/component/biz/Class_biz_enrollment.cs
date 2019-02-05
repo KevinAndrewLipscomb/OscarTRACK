93710b68062f9c9a3f61dc5224f720ea50e082ea
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2019-02-05 15:31:08 +0000

    Revised to skip the call to IssueForElectiveDeparture if exit_survey_url is missing or empty in Web.config.

--- trunk/OscarDotNet/component/biz/Class_biz_enrollment.cs
+++ trunk/OscarDotNet/component/biz/Class_biz_enrollment.cs
@@ -282,7 +282,7 @@ namespace Class_biz_enrollment
           biz_notifications.IssueForNewEnrollmentLevel(member_id, first_name, last_name, cad_num, new_level_description, effective_date.ToString("yyyy-MM-dd"), note);
           }
         //
-        if ((new ArrayList() {"Transferring","Unknown","Resigned","Retired"}).Contains(new_level_description))
+        if ((ConfigurationManager.AppSettings["exit_survey_url"] != null) && (ConfigurationManager.AppSettings["exit_survey_url"].Length > 0) && new ArrayList() {"Transferring","Unknown","Resigned","Retired"}.Contains(new_level_description))
           {
           biz_notifications.IssueForElectiveDeparture(member_id, first_name, last_name, cad_num, new_level_description, effective_date.ToString("yyyy-MM-dd"), note);
           }
