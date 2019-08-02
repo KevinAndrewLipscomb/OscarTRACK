59ec445889f4fd5f9476d611e81d49704426d5a7
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2019-08-02 18:08:26 +0000

    Revised to correct sorting.

--- trunk/OscarDotNet/js/ErrorHandler.js
+++ trunk/OscarDotNet/js/ErrorHandler.js
@@ -1,12 +1,12 @@
 function EndRequestHandler(sender, args)
   {
-  if (args.get_error() != undefined)
+  if (args.get_error() !== undefined)
     {
     var outgoing_error_message = incoming_error_message = args.get_error().message;
     if(
-        (incoming_error_message == "Sys.WebForms.PageRequestManagerParserErrorException: The message received from the server could not be parsed.")
+        incoming_error_message === "Sys.WebForms.PageRequestManagerParserErrorException: The message received from the server could not be parsed."
       ||
-        (incoming_error_message.indexOf("Sys.WebForms.PageRequestManagerServerErrorException: ") == 0)
+        incoming_error_message.indexOf("Sys.WebForms.PageRequestManagerServerErrorException: ") === 0
       )
       {
       outgoing_error_message = "The server sent something unexpected.  If the web application continues to behave abnormally, try logging out, then logging back in again.\n\nSorry for the inconvenience.";
