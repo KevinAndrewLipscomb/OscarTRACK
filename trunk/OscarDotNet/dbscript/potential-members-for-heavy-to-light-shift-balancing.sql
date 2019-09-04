ea65583c31eaa3a78f09c7d11c37ecce74e9b096
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2019-09-04 14:09:39 +0000

    Revised to ignore members whose primary affiliation is not with a volunteer rescue squad.

--- trunk/OscarDotNet/dbscript/potential-members-for-heavy-to-light-shift-balancing.sql
+++ trunk/OscarDotNet/dbscript/potential-members-for-heavy-to-light-shift-balancing.sql
@@ -5,10 +5,10 @@ from schedule_assignment heavy
   join member on (member.id=heavy.member_id)
   join schedule_assignment light on (light.member_id=heavy.member_id)
   join shift light_shift on (light_shift.id=light.shift_id)
-where member.agency_id <> 0
-  and heavy.nominal_day = '2019-08-27'                                                        -- !!! ADJUST !!!
-  and heavy_shift.name = 'NIGHT'                                                               -- !!! ADJUST !!!
+where member.agency_id between 1 and 199
+  and heavy.nominal_day = '2019-09-21'                                                        -- !!! ADJUST !!!
+  and heavy_shift.name = 'DAY'                                                               -- !!! ADJUST !!!
   and heavy.be_selected
   and NOT light.be_selected
-  and light.nominal_day = '2019-08-26'                                                        -- !!! ADJUST !!!
+  and light.nominal_day = '2019-09-26'                                                        -- !!! ADJUST !!!
   and light_shift.name = 'NIGHT'                                                               -- !!! ADJUST !!!
