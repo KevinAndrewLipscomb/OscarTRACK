c216d09dbc328c16b47a749db11114c3853ea511
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2020-05-22 18:57:16 +0000

    Revised ErrorHandler.js to be more robust.  Recompiled with new ki.dll (Added code to inject a custom HTTP header for the partial postback error handler to check, when a session is unexpectedly interrupted.)

--- trunk/OscarDotNet/js/ErrorHandler.js
+++ trunk/OscarDotNet/js/ErrorHandler.js
@@ -5,11 +5,22 @@ function EndRequestHandler(sender, args)
   //
   if (args.get_error() !== null) // There has been an error.
     {
-    const APP_HANDLED_ERROR_MESSAGE_INDICATOR = "Sys.WebForms.PageRequestManagerServerErrorException: -=:AppHandledMessage:=-\n";
+    const APP_HANDLED_ERROR_MESSAGE_LINE = "Sys.WebForms.PageRequestManagerServerErrorException: -=:App-Handled-Message:=-\n"; // must correspond to ki_web_ui.common.APP_HANDLED_ASYNC_POST_BACK_ERROR_MESSAGE_MARK
+    //
     let outgoing_error_message = incoming_error_message = String(args.get_error().message);
-    if (incoming_error_message.startsWith(APP_HANDLED_ERROR_MESSAGE_INDICATOR))
+    let custom_response_header_value = args.get_response().getResponseHeader("KiAspdotnetFramework-Pragma");
+    //
+    if (custom_response_header_value === "Session-Interrupted")
       {
-      outgoing_error_message = incoming_error_message.replace(APP_HANDLED_ERROR_MESSAGE_INDICATOR,"");
+      outgoing_error_message = "Your session has been interrupted.  Please log out, then log back in again.";
+      }
+    else if (custom_response_header_value === "App-Handled-Message") // we've figured out how to implement the preferred HTTP header based indication mechanism
+      {
+      outgoing_error_message = incoming_error_message.replace("Sys.WebForms.PageRequestManagerServerErrorException: ","");
+      }
+    else if (incoming_error_message.startsWith(APP_HANDLED_ERROR_MESSAGE_LINE)) // we're still using the suboptimal message-based indication mechanism
+      {
+      outgoing_error_message = incoming_error_message.replace(APP_HANDLED_ERROR_MESSAGE_LINE,"");
       }
     else if (incoming_error_message.startsWith("Sys.WebForms.PageRequestManagerParserErrorException: The message received from the server could not be parsed."))
       {
