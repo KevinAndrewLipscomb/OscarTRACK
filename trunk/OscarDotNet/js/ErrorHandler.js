c4c11f140b3897fac17124747dde9af27baf9e5e
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2020-05-22 00:16:28 +0000

    Corrected long-standing problem in ErrorHandler.js made evident by turning compilation debug off in production.

--- trunk/OscarDotNet/js/ErrorHandler.js
+++ trunk/OscarDotNet/js/ErrorHandler.js
@@ -11,7 +11,7 @@ function EndRequestHandler(sender, args)
       {
       outgoing_error_message = incoming_error_message.replace(APP_HANDLED_ERROR_MESSAGE_INDICATOR,"");
       }
-    else if (incoming_error_message === "Sys.WebForms.PageRequestManagerParserErrorException: The message received from the server could not be parsed.")
+    else if (incoming_error_message.startsWith("Sys.WebForms.PageRequestManagerParserErrorException: The message received from the server could not be parsed."))
       {
       outgoing_error_message = "The server sent something unexpected.  If the web application continues to behave abnormally, try logging out, then logging back in again.\n\nSorry for the inconvenience.";
       }
