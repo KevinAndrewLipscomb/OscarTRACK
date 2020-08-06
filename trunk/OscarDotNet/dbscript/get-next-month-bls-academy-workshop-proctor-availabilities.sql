993b2787b82d8a2b345d32505d2ba190535f3963
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2020-08-06 20:19:35 +0000

    Creation.

---
+++ trunk/OscarDotNet/dbscript/get-next-month-bls-academy-workshop-proctor-availabilities.sql
@@ -0,0 +1,15 @@
+SELECT nominal_day
+, name
+, last_name
+, first_name
+, cad_num
+, phone_num
+, email_address
+FROM special_event_avail
+  join special_event_shift on (special_event_shift.id=special_event_avail.shift_id)
+  join special_event on (special_event.id=special_event_shift.special_event_id)
+  join oscardotnetdb.member m on (m.id=special_event_avail.odnmid)
+where special_event.description like 'BLS Academy % workshop %'
+  and MONTH(nominal_day) = MONTH(ADDDATE(CURDATE(),INTERVAL 1 MONTH))
+  and m.be_bls_academy_proctor
+order by nominal_day, name, cad_num
