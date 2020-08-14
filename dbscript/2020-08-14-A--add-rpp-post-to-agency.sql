3c476069c70b5ccec900dde8d24b2cb058efcabf
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2020-08-14 16:44:36 +0000

    Added RPP (Respiratory Protection Program) as an assignable post.

---
+++ dbscript/2020-08-14-A--add-rpp-post-to-agency.sql
@@ -0,0 +1,6 @@
+START TRANSACTION
+;
+insert ignore agency (id,short_designator,medium_designator,long_designator,be_ems_post) values
+(1001,"RPP","Resp Pro Program","Respiratory Protection Program",1)
+;
+COMMIT
