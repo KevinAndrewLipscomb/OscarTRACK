c5cd9c9e3bf00354c5a7a78e9ed0716c5320558e
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2022-12-02 01:06:04 +0000

    Revised for compatibility w/MySQL 5.6.

--- dbscript/2022-11-05-A--create-table-scenes_reached.sql
+++ dbscript/2022-11-05-A--create-table-scenes_reached.sql
@@ -4,7 +4,7 @@ CREATE TABLE `scenes_reached`
   `agency_id` INT UNSIGNED DEFAULT 0,
   `address` VARCHAR(63),
   PRIMARY KEY (`id`),
-  INDEX `agency_id` (`agency_id` ASC) VISIBLE,
+  INDEX `agency_id` (`agency_id` ASC),
   CONSTRAINT `scenes_reached_agency_id` FOREIGN KEY (`agency_id`) REFERENCES `agency` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
   )
 ;
