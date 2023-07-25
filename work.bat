ad654b2e4b3ddfc2495313650012f79ed3750271
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2023-07-25 19:06:04 -0400

    Converted .svn reference to .git.

--- work.bat
+++ work.bat
@@ -2,6 +2,6 @@ REM
 REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\OscarDotNet"
-start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.svn
+start /max explorer /e,/select,C:\Inetpub\wwwroot\OscarDotNet\.git
 start /max OscarDotNet.sln
 IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
